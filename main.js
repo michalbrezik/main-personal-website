5dce07edaf5c37cca8276c4193e71769a3940fa9
BuildTools <06michal.br@seznam.cz>
2025-08-27 20:14:15 +0200

    button not working on the first click fix

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function darkMode(){
     document.getElementById("dark-mode").onclick = function(){
         if(document.body.style.backgroundColor == "black"){
             document.body.style.backgroundColor = "white";
-           document.body.style.color="black";
+            document.body.style.color="black";
         }
         else{
             document.body.style.backgroundColor = "black";
