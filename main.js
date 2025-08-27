f492ecd2946ea4dc4e3418cf1c8abda38f130401
BuildTools <06michal.br@seznam.cz>
2025-08-27 19:17:34 +0200

    main website components test

---
+++ main.js
@@ -0,0 +1,13 @@
+function darkMode(){
+    document.getElementById("dark-mode").onclick = function(){
+        if(document.body.style.backgroundColor == "black"){
+            document.body.style.backgroundColor = "white";
+           document.body.style.color="black";
+        }
+        else{
+            document.body.style.backgroundColor = "black";
+            document.body.style.color= "white";
+        }
+    }
+}
+document.addEventListener(darkMode());
