a72e90bd9f437baed1324ff89b1584cc7915551d
BuildTools <06michal.br@seznam.cz>
2025-08-27 21:33:02 +0200

    Added social links and deleted unneeded files

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 function darkMode(){
     document.getElementById("dark-mode").onclick = function(){
-        if(document.body.style.backgroundColor == "black"){
+        if(document.body.style.backgroundColor == "black")
+        {
+            //light mode
             document.body.style.backgroundColor = "white";
             document.body.style.color="black";
+             var elements = document.getElementsByClassName('socialLinksDiv');
+                for (var i in elements) 
+                    {
+                     if (elements.hasOwnProperty(i)) 
+                         {
+                          elements[i].className = 'socialLinksDivLightAlt';
+                           }
+                    }
         }
         else{
+            //dark mode
             document.body.style.backgroundColor = "black";
             document.body.style.color= "white";
+                var elements = document.getElementsByClassName('socialLinksDivLightAlt');
+                for (var i in elements) 
+                    {
+                     if (elements.hasOwnProperty(i)) 
+                         {
+                          elements[i].className = 'socialLinksDiv';
+                           }
+                    }
         }
     }
 }
-document.addEventListener(darkMode());
+document.addEventListener("event",darkMode());
